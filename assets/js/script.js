0f38c6985c9a4c84f34b95903abd0eb226926dd2
Ivan Serikov <vandomeri@gmail.com>
2021-02-09 21:45:06 +0300

    1

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    arrows: false,
+    autoplay: true,
+  });
+});
+
+$(document).mouseleave(function () {
+  $("#cls_ctnr").show();
+});
+
+
+
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+  let links = menu.find('.burger-menu_link');
+  let overlay = menu.find('.burger-menu_overlay');
+
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass('burger-menu_active');
+
+    if (menu.hasClass('burger-menu_active')) {
+      $('body').css('overlow', 'hidden');
+    } else {
+      $('body').css('overlow', 'visible');
+    }
+  }
+}
+
+burgerMenu('.burger-menu');
